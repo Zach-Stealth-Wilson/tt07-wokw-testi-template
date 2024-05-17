90db833bb83a9f6649c1f90cd3e5bff1b4269c9c
Zach-Stealth-Wilson <123966207+Zach-Stealth-Wilson@users.noreply.github.com>
2024-05-17 12:34:13 -0500

    Add files via upload

---
+++ test/tt_um_wokwi_398151379603008513.v
@@ -0,0 +1,23 @@
+/* Automatically generated from https://wokwi.com/projects/398151379603008513 */
+
+`default_nettype none
+
+// verilator lint_off UNUSEDSIGNAL
+// verilator lint_off PINCONNECTEMPTY
+
+module tt_um_wokwi_398151379603008513(
+  input [7:0] io_in,
+  output [7:0] io_out
+);
+  wire net1 = 1'b0;
+  wire net2 = 1'b1;
+  wire net3 = 1'b1;
+  wire net4;
+  wire net5;
+
+  and_cell and1 (
+    .a (net4),
+    .b (net2),
+    .out (net5)
+  );
+endmodule
